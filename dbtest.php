e4494b339aabdc9564f2c52014bf26e63b2cb949
Franco Chiellino <franco@chiellino.ch>
2016-12-30 12:14:17 +0100

    new version of query

--- dbtest.php
+++ dbtest.php
@@ -4,18 +4,33 @@ $dbport = getenv("MYSQL_SERVICE_PORT");
 $dbuser = getenv("MYSQL_USER");
 $dbpwd = getenv("MYSQL_PASSWORD");
 $dbname = getenv("MYSQL_DATABASE");
-$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
-if ($connection->connect_errno) {
-    printf("Connect failed: %s\n", $mysqli->connect_error);
-    exit();
-} else {
-    printf("Connected to the database");
-    $query = "SELECT * from users";
-    $rs = mysql_query($query);
-    while ($row = mysql_fetch_assoc($rs)) {
-      echo $row['ID'] . " " . $row['name'] . "\n";
-    }
-    mysql_close();
+
+
+$db_link = mysqli_connect (
+                     $dbhost, 
+                     $dbuser, 
+                     $dbpwd, 
+                     $dbname
+                    );
+ 
+$sql = "SELECT * FROM users";
+ 
+$db_erg = mysqli_query( $db_link, $sql );
+if ( ! $db_erg )
+{
+  die('query failure: ' . mysqli_error());
 }
-$connection->close();
+ 
+echo '<table border="1">';
+while ($zeile = mysqli_fetch_array( $db_erg, MYSQL_ASSOC))
+{
+  echo "<tr>";
+  echo "<td>". $zeile['id'] . "</td>";
+  echo "<td>". $zeile['name'] . "</td>";
+  echo "</tr>";
+}
+echo "</table>";
+ 
+mysqli_free_result( $db_erg );
+
 ?>
